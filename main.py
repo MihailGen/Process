fa565b07efe61a653705dec2a04c86e38f7c08b4
Mihail Genzik <genziks@inbox.lv>
2024-06-11 10:56:52 +0300

    в параметрах вызова функции i заменено на значение

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     process = []
     numbers = [1, 3, 5, 7, 9, 11]
     for i in range(len(numbers)):
-        p = multiprocessing.Process(target=square, args=(i, return_dict))
+        p = multiprocessing.Process(target=square, args=(numbers[i], return_dict))
         process.append(p)
         p.start()  # запускам процессы в цикле
 
